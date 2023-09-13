4304f779fb1fa78c17c70280a4f5defac77d7ab1
saurabhlm10 <saurabhlm10@gmail.com>
2023-09-13 19:52:07 +0530

    check

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var http = require('http');
+const os = require('os')
+
+console.log(os.cpus().length)
+
 http.createServer(function (req, res) {
+
     console.log(`Just got a request at ${req.url}!`)
     res.write('Yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
+
+
