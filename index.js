b997bc9027b36249fbd0e2166b1b5f5347a66095
saurabhlm10 <saurabhlm10@gmail.com>
2023-09-13 19:55:11 +0530

    added

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Yo!');
     res.end();
-}).listen(process.env.PORT || 3000);
+}).listen(process.env.PORT || 3000, () => console.log('server is listening on port 3000'));
 
 
