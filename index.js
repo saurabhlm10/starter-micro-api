f9d2bb215b377494ae75a132138a1e7f0fa23966
saurabhlm10 <saurabhlm10@gmail.com>
2023-09-19 22:00:01 +0530

    testing date

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var http = require('http');
-const os = require('os')
+var http = require("http");
+const os = require("os");
 
-console.log('CORES', os.cpus().length)
+console.log("CORES", os.cpus().length);
+console.log(
+  "DATE",
+  new Date().toLocaleString(undefined, { timeZone: "Asia/Kolkata" })
+);
 
-http.createServer(function (req, res) {
-
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+http
+  .createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`);
+    res.write("Yo!");
     res.end();
-}).listen(process.env.PORT || 3000, () => console.log('server is listening on port 3000'));
-
-
+  })
+  .listen(process.env.PORT || 3000, () =>
+    console.log("server is listening on port 3000")
+  );
